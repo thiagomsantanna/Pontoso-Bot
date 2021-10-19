07423e7c72d5812013c3b772449b5b9c0504f2c2
thiagomsantanna <thiagomsantanna11@gmail.com>
2021-10-18 22:39:39 -0300

    interaction --> messageCreate

--- index.js
+++ index.js
@@ -1,48 +1,107 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, CommandInteractionOptionResolver } = require('discord.js');
 const result = require('dotenv').config({ path: '.env' })
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
+// intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"]
+// intents: [Intents.FLAGS.GUILDS]
 client.once('ready', () => {
   console.log('Ready!');
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
+client.on('messageCreate', async mensagem => {
 
-  const { commandName } = interaction;
-
-  // if (commandName === 'ping') {
-  //   await interaction.reply('Pong!');
-  // } else if (commandName === 'server') {
-  //   await interaction.reply('Server info.');
-  // } else if (commandName === 'user') {
-  //   await interaction.reply('User info.');
-  // }
   var todaysDay = `${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`
   var now = `${new Date().getHours()}:${new Date().getMinutes()}`
-  //var message;
+  const canalId = '898614312933920790';
 
-  switch (commandName) {
-    case 'ping':
-      message = await interaction.reply({ content: `>>> ${todaysDay} - ${now}`, fetchReply: true });//console.log(interaction.channelId)
-      console.log(message.id);
+  switch (mensagem.content) {
+    case '!cheguei':
+      pontoMsg = await mensagem.reply({ content: `>>> ${todaysDay} - ${now} Início`, fetchReply: true });
+      //message = await interaction.reply('dale').fetchReply();
       //console.log(interaction);
-      //canalID = interaction.channelId;
-      //console.log(canalID);
+      console.log(pontoMsg.id);
+      console.log(mensagem)
+      console.log('\n')
+      console.log(pontoMsg)
+
       break;
-    case 'server':
-      await interaction.reply('Server info.');
-      message = await message.edit({ content: `${message.content}\n${todaysDay} - ${now}`, fetchReply: true });
-      console.log(message.id);
-      //let testt = await canalID.send('sifoda');
-      //console.log(testt);
-      //await interaction.send
+    case '!almoco':
+      //interaction.fetchReply()
+      // message = 
+      //await interaction.reply(`${interaction.user.username} toma aqui essa coquinha gelada pra ti`)
+      //canalId.send('sifoda').then(message => console.log(message));
+      pontoMsg = await pontoMsg.edit({ content: `${pontoMsg.content}\n${todaysDay} - ${now} Intervalo`, fetchReply: true });
+      //message = await message.edit({ content: `${message.content}\n${todaysDay} - ${now} Intervalo`, fetchReply: true });
+      console.log(pontoMsg.id);
+
       break;
-    case 'user':
-      await interaction.reply(`User info.\n ${interaction.user.avatar}\n\n\n ${interaction.user.avatarURL({})}`);
+    case '!voltei':
+      //await interaction.reply(`User info.\n ${interaction.user.avatar}\n\n\n ${interaction.user.avatarURL({})}`);
+      //await interaction.reply(`${interaction.user.username} tá de buxin chei?`)
+      pontoMsg = await pontoMsg.edit({ content: `${pontoMsg.content}\n${todaysDay} - ${now} Retorno`, fetchReply: true });
+      //await interaction.fetchMessage(message).then(message => console.log(`${message}`));
+      console.log(pontoMsg.id);
+
+      break;
+    case '!tchau':
+      pontoMsg = await pontoMsg.edit({ content: `${pontoMsg.content}\n${todaysDay} - ${now} Saída`, fetchReply: true });
+      console.log(pontoMsg.id)
+
       break;
   }
+
+  //msg.react('👍');
+  //msg.react('👎');
+  //msg.react('😎');
+
+  //console.log(filter);
+
+  //console.log(mensagem);
 });
+
+// client.on('interactionCreate', async interaction => {
+
+//   if (!interaction.isCommand()) return;
+
+//   const { commandName } = interaction;
+
+//   var todaysDay = `${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`
+//   var now = `${new Date().getHours()}:${new Date().getMinutes()}`
+//   const canalId = '898614312933920790';
+
+//   switch (commandName) {
+//     case 'cheguei':
+//       message = await interaction.reply({ content: `>>> ${todaysDay} - ${now} Início`, fetchReply: true });
+//       //message = await interaction.reply('dale').fetchReply();
+//       //console.log(interaction);
+//       console.log(message.id);
+
+//       break;
+//     case 'almoco':
+//       //interaction.fetchReply()
+//       // message = 
+//       //await interaction.reply(`${interaction.user.username} toma aqui essa coquinha gelada pra ti`)
+//       canalId.send('sifoda').then(message => console.log(message));
+//       // message = await interaction.editReply({content: `${message.content}\n${todaysDay} - ${now} Intervalo`, fetchReply: true});
+//       //message = await message.edit({ content: `${message.content}\n${todaysDay} - ${now} Intervalo`, fetchReply: true });
+//       //console.log(message.id);
+
+//       break;
+//     case 'voltei':
+//       //await interaction.reply(`User info.\n ${interaction.user.avatar}\n\n\n ${interaction.user.avatarURL({})}`);
+//       //await interaction.reply(`${interaction.user.username} tá de buxin chei?`)
+//       await interaction.isCommand;
+//       message = await message.edit({content: `${message.content}\n${todaysDay} - ${now} Retorno`, fetchReply: true});
+//       //await interaction.fetchMessage(message).then(message => console.log(`${message}`));
+//       //console.log(message.id);
+
+//       break;
+//     case 'tchau':
+//       message = await message.edit({ content: `${message.content}\n${todaysDay} - ${now} Saída`, fetchReply: true });
+//       console.log(message.id)
+
+//       break;
+//   }
+// });
 
 client.login(process.env.BOT_TOKEN);

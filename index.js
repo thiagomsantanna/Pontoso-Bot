ee25325cd925554d11021b07d01f7421389ed71f
thiagomsantanna <thiagomsantanna11@gmail.com>
2021-10-31 22:00:20 -0300

    verificação para 1 ponto por dia

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ client.on('messageCreate', async mensagem => {
                 dataUltimaMsg = new Date(_msgPontoParaColocarAlmoco.pontoMessage.createdTimestamp).setHours(0, 0, 0, 0);
 
                 if (diaTodo > dataUltimaMsg) {
-                    await mensagem.channel.send('Esqueceu de bater o ponto hoje né, bonitão?');
+                    //await mensagem.channel.send('Esqueceu de bater o ponto hoje né, bonitão?');
+                    await mensagem.reply({ content: 'Esqueceu de bater o ponto hoje né, bonitão?', ephemeral: true});
                 }
                 else {
                     await _msgPontoParaColocarAlmoco.pontoMessage.edit({ content: `${_msgPontoParaColocarAlmoco.pontoMessage.content}\n${todaysDay} - ${now} Intervalo`, fetchReply: true });
@@ -44,7 +45,8 @@ client.on('messageCreate', async mensagem => {
                 }
             }
             else {
-                await mensagem.channel.send('Vai pro almoço direto?')
+                //await mensagem.channel.send('Vai pro almoço direto?')
+                await mensagem.reply({ content: 'Vai pro almoço direto?', ephemeral: true});
                 break;
             }
 
@@ -60,7 +62,8 @@ client.on('messageCreate', async mensagem => {
                 dataUltimaMsg = new Date(_msgPontoParaColocarRetorno.pontoMessage.createdTimestamp).setHours(0, 0, 0, 0);
 
                 if (diaTodo > dataUltimaMsg) {
-                    await mensagem.channel.send('Esqueceu de bater o ponto hoje né, bonitão?');
+                    //await mensagem.channel.send('Esqueceu de bater o ponto hoje né, bonitão?');
+                    await mensagem.reply({ content: 'Esqueceu de bater o ponto hoje né, bonitão?', ephemeral: true});
                 }
                 else {
                     await _msgPontoParaColocarRetorno.pontoMessage.edit({ content: `${_msgPontoParaColocarRetorno.pontoMessage.content}\n${todaysDay} - ${now} Retorno`, fetchReply: true });
@@ -72,7 +75,8 @@ client.on('messageCreate', async mensagem => {
                 }
             }
             else {
-                await mensagem.channel.send('Voltou da onde? Nem almoçou direito meu fi, calmai que vovó vai fazer um bolinho pra vc viu')
+                //await mensagem.channel.send('Voltou da onde? Nem almoçou direito meu fi, calmai que vovó vai fazer um bolinho pra vc viu');
+                await mensagem.reply({ content: 'Voltou da onde? Nem almoçou direito meu fi, calmai que vovó vai fazer um bolinho pra vc viu', ephemeral: true});
                 break;
             }
 
@@ -101,7 +105,8 @@ client.on('messageCreate', async mensagem => {
                 }
             }
             else {
-                await mensagem.channel.send('Nem chegou e já ta saindo fora?')
+                //await mensagem.channel.send('Nem chegou e já ta saindo fora?')
+                await mensagem.reply({content: 'Nem chegou e já ta saindo fora?', ephemeral: true})
                 await mensagem.author.send('https://www.youtube.com/watch?v=6qkVt3AywOk');
                 break;
             }
@@ -110,6 +115,9 @@ client.on('messageCreate', async mensagem => {
     }
 });
 
+//variavel que salva a interaction timeStamp
+var _interacoes = [];//1634592789 //1635718484860
+//{ userID: '222090790627704832', horarioDia: 1635718484860 }
 client.on('interactionCreate', async interaction => {
 
     if (!interaction.isCommand()) return;
@@ -127,29 +135,104 @@ client.on('interactionCreate', async interaction => {
 
     if (commandName === 'ponto') {
 
-        pontoMessage = await interaction.reply({ content: `>>> <@${interaction.user.id}>\n${todaysDay} - ${now} Início`, fetchReply: true });
+        var diaTodo = new Date().setHours(0, 0, 0, 0);
 
-        let _firstUserMsg = [{ interaction, pontoMessage }];
+        var _interacao = { userID: interaction.user.id, horarioDia: interaction.createdTimestamp}
+        console.log(_interacao);
 
-        messages.push(Object.assign(..._firstUserMsg));
+        let _interacoesDoUser = _interacoes.filter(tm => tm.userID === interaction.user.id)
+        console.log(_interacoesDoUser);
+        _interacoesDoUser.reverse();
 
-        //pontoMessage.react(emojis[numeroAleatorio(emojis.length)]);
-        var climaHoje = await clima.pegaClimaRioPreto();
-        pontoMessage.react(climaHoje);
+        let _ultimoPontoBatidoPeloUsuario = _interacoesDoUser.find(last => last.userID === interaction.user.id);
+        
 
-        console.log(pontoMessage.id);
+        if (_interacoesDoUser.length > 0) {
+
+            _dataUltimoPonto = new Date(_ultimoPontoBatidoPeloUsuario.horarioDia).setHours(0, 0, 0, 0);
+            
+            console.log(_dataUltimoPonto);
+
+            if (diaTodo > _dataUltimoPonto) {
+                //interaction.reply({content: 'Bate o ponto ai cumade', ephemeral: true});
+                interaction.channel.send('bom te ver denovo');
+
+                pontoMessage = await interaction.reply({ content: `>>> <@${interaction.user.id}>\n${todaysDay} - ${now} Início`, fetchReply: true });
+                
+                _interacao = {_interacao, pontoMessage};
+                console.log('*********************************')
+                console.log(_interacao)
+                
+                let _firstUserMsg = [{ interaction, pontoMessage }];
+                // _interacoes.push(Object.assign(..._firstUserMsg));
+                _interacoes.push(_interacao);
+                messages.push(Object.assign(..._firstUserMsg));
+        
+                //pontoMessage.react(emojis[numeroAleatorio(emojis.length)]);
+                var climaHoje = await clima.pegaClimaRioPreto();
+                pontoMessage.react(climaHoje);
+        
+                console.log(pontoMessage.id);
+            } else {
+                interaction.reply({ content: 'Ô oreia, vai bate o ponto duas vezes? isso daí pra mim é Hack.', ephemeral: true})
+            }
+        } else {
+
+            pontoMessage = await interaction.reply({ content: `>>> <@${interaction.user.id}>\n${todaysDay} - ${now} Início`, fetchReply: true });
+        
+            let _firstUserMsg = [{ interaction, pontoMessage }];
+            // _interacoes.push(Object.assign(..._firstUserMsg));
+            _interacoes.push(_interacao);
+            messages.push(Object.assign(..._firstUserMsg));
+    
+            //pontoMessage.react(emojis[numeroAleatorio(emojis.length)]);
+            var climaHoje = await clima.pegaClimaRioPreto();
+            pontoMessage.react(climaHoje);
+    
+            console.log(pontoMessage.id);
+        }
+ 
     }
 
     if (commandName == 'esquecipapa') {
 
-        var almocoDoCara = interaction.options.getString('horario');
+        //verificar se tem algo na variavel de interactions
+        //se tiver faz certin
+        //se não avisa que precisa bater o ponto
+        let _mensagensDePontoDoUsuario = messages.filter(fm => fm.interaction.user.id === interaction.user.id);
+        _mensagensDePontoDoUsuario.reverse();
 
-        if (almocoDoCara.includes(':')) {
-            await interaction.reply({ content: 'êêêê.. oreiudo memo hein!', ephemeral: true});
-            pontoMessage.edit({ content: `${pontoMessage.content}\n${todaysDay} - ${almocoDoCara} Intervalo`, fetchReply: true })
-        }else{
-            await interaction.reply({ content: 'O modelo de horas utilizado é => **00:00**\n Te manca oreião', ephemeral: true});
+        let _UltimaMsgDePonto = _mensagensDePontoDoUsuario.find(last => last.interaction.user.id === interaction.user.id);
+        
+        if (_UltimaMsgDePonto) {
+            
+            dataUltimaMsg = new Date(_UltimaMsgDePonto.pontoMessage.createdTimestamp).setHours(0,0,0,0);
+
+            if(diaTodo > dataUltimaMsg) {
+                await interaction.reply({ content: 'Pô esqueceu do ponto hoje Zé? ta marcando, usa o `/esqueciponto` ai vai', ephemeral: true})
+            } else {
+                var almocoDoCara = interaction.options.getString('horario');
+
+                if (almocoDoCara.includes(':')) {
+                    await interaction.reply({ content: 'êêêê.. oreiudo memo hein!', ephemeral: true});
+                    pontoMessage.edit({ content: `${pontoMessage.content}\n${todaysDay} - ${almocoDoCara} Intervalo`, fetchReply: true })
+                }else{
+                    await interaction.reply({ content: 'O modelo de horas utilizado é => **00:00**\n Te manca oreião', ephemeral: true});
+                }
+            }
+        } else {
+            await interaction.reply({ content: 'Vai pro almoço direto?', ephemeral: true});
         }
+
+
+        // var almocoDoCara = interaction.options.getString('horario');
+
+        // if (almocoDoCara.includes(':')) {
+        //     await interaction.reply({ content: 'êêêê.. oreiudo memo hein!', ephemeral: true});
+        //     pontoMessage.edit({ content: `${pontoMessage.content}\n${todaysDay} - ${almocoDoCara} Intervalo`, fetchReply: true })
+        // }else{
+        //     await interaction.reply({ content: 'O modelo de horas utilizado é => **00:00**\n Te manca oreião', ephemeral: true});
+        // }
 
     }
 

fa0cf7d499e318702a012f503556e44dbd44a7f8
thiagomsantanna <thiagomsantanna11@gmail.com>
2021-11-17 20:13:02 -0300

    correção para ci pass

--- pegaDataHora.js
+++ pegaDataHora.js
@@ -25,7 +25,7 @@ function pegaData(){
 
     let dataHoras = datasHorasAgora();
 
-    return `9999999999${dataHoras.diaHoje}/${dataHoras.mesHoje}/${dataHoras.anoHoje}`;
+    return `${dataHoras.diaHoje}/${dataHoras.mesHoje}/${dataHoras.anoHoje}`;
 }
 
 module.exports = {

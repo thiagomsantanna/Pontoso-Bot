c18738d1be89ef21510b43fd5cfb6f792960855c
thiagomsantanna <thiagomsantanna11@gmail.com>
2022-02-23 20:39:20 -0300

    deploy de comandos para a guild que convidar o ponto

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,21 +1,19 @@
-const { SlashCommandBuilder, SlashCommandAssertions } = require('@discordjs/builders');
+require('dotenv').config({ path: '.env' })
+const fs = require('fs');
+const path = require('path');
 const { Routes } = require('discord-api-types/v9');
 const { REST } = require('@discordjs/rest');
 const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
 
-const commands = [
-    //new SlashCommandBuilder().setName('ponto').setDescription('Marca o horário de Início de trabalho.'),
-    new SlashCommandBuilder().setName('ponto').setDescription('Marca o horário de Início de trabalho.').addStringOption(option => 	option.setName('horario').setDescription('Coloca um horário especifíco caso desejado').setRequired(false)),
-    new SlashCommandBuilder().setName('esquecidoalmoco').setDescription('Pros esquecidos que esqueceram de bater o Almoço').addStringOption(option => 
-        option.setName('horario')
-        .setDescription('Horário no qual saiu para almoço,\n utilize o modelo => "00:00" com dois pontos')
-        .setRequired(true)),
-    new SlashCommandBuilder().setName('esqueci').setDescription('Pros esquecidos que esqueceram de bater o ponto').addStringOption(option => 
-        option.setName('horario')
-        .setDescription('Horário no qual deveria ter batido o ponto,\n utilize o modelo => "00:00" com dois pontos')
-        .setRequired(true))
-]
-    .map(command => command.toJSON());
+
+const commandFiles = fs.readdirSync(path.resolve(__dirname, '../commands/')).filter(file => file.endsWith('.js'));
+let commands = [];
+
+for(const file of commandFiles){
+    const command = require(`../commands/${file}`);
+    commands.push(command.data.toJSON());
+};
+
 
 module.exports ={
     name: 'guildCreate',
@@ -32,7 +30,7 @@ module.exports ={
             const test = {
                 title: "Obrigado por convidar o Pontoso ao seu servidor!",
                 color: 9515056,
-                description: "\n\nRecomendamos o uso do bot em um canal separado assim como esse para evitar de poluir outros canais e facilitar o uso, porém, os comandos funcionarão em todos os canais sem problemas, sinta-se à vontade de usar a sua maneira!\n\n\nOs comandos para uso são:\n\n   `/ponto` - Para iniciar o ponto e utilize os emojis abaixo do Ponto para marcar os momentos de intervalo e saída do expediente. Também é possível passar um horário específico de ínicio do ponto utilizando a opção de `horário` junto do `/ponto`.\n\n",
+                description: "\n\nRecomendamos o uso do bot em um canal separado assim como esse para evitar de poluir outros canais e facilitar o uso, porém, os comandos funcionarão em todos os canais sem problemas, sinta-se à vontade de usar a sua maneira!\n\n\nOs comandos para uso são:\n\n   `/ponto` - Para iniciar o ponto e utilize os emojis abaixo do Ponto para marcar os momentos de intervalo e saída do expediente. Também é possível passar um horário específico de ínicio do ponto utilizando a opção de `horário` junto do `/ponto`.\n\n`/esquecidoalmoco` - Para caso esqueça de marcar o horário de Intervalo na hora exata e necessite passar um horário específico. Utilize a opção de `horário` para passar a hora.",
                 thumbnail: {
                        url: "https://i.pinimg.com/originals/d5/e8/88/d5e8885841aaf131b836fa4f955ac2fa.gif"
                 },
@@ -41,8 +39,8 @@ module.exports ={
             await pontoChannel.send({ content: "@everyone", embeds: [test] });
         }
         
-        rest.put(Routes.applicationGuildCommand(process.env.CLIENT_ID, guild.id), {body: commands })
-            .then(() => console.log(`Comandos adicionados ao servidor ${guild.name}`))
-            .catch(console.error);
+        rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, guild.id), { body: commands })
+	        .then(() => console.log(`Commandos deployados para: ${guild.name}`))
+	        .catch(console.error);
     }
 };

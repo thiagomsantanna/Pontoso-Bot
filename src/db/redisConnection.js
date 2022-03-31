65a1c54c826500cdd002acc17896dc07f6194666
thiagomsantanna <thiagomsantanna11@gmail.com>
2022-03-30 21:25:31 -0300

    refac if else na conexão

--- src/db/redisConnection.js
+++ src/db/redisConnection.js
@@ -1,25 +1,18 @@
 const redis = require("redis");
-require('dotenv').config({ path: '.env' });
+require("dotenv").config({ path: ".env" });
 
 let client;
 
-if(process.env.REDIS_URL){
-    //connect no redis no heroku
-    client = redis.createClient({
-        url: process.env.REDIS_URL,
-    })
-} else {
-    //connect no redis localhost
-    client = redis.createClient();
-}
+process.env.REDIS_URL
+  ? (client = redis.createClient({ url: process.env.REDIS_URL }))
+  : (client = redis.createClient());
 
 client.on("error", (err) => {
-    console.log("Error - " + err);
+  console.log("Error - " + err);
 });
 
-client.on('connect', () => {
-    console.log('Conectado!');
+client.on("connect", () => {
+  console.log("Conectado!");
 });
 
 module.exports = client;
-// module.exports = { salvaPonto, salvaMessage, getChave, getPontosDoUsuario, apagaPontoAntigo };

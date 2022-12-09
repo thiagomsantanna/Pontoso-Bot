6fb756a256717b713c89cc2032a472fe38428b44
thiagomsantanna <thiagomsantanna11@gmail.com>
2022-12-09 02:17:40 -0300

    refac: Refactoring all ponto command code to a more clean and readable approach, focused on better readability

--- src/commands/ponto.js
+++ src/commands/ponto.js
@@ -1,80 +1,62 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const dataUtil = require("../utils/DataHoraUtils.js");
-const redis = require("../utils/RedisUtils.js");
-const discord = require("../utils/PontoUtils.js");
+const { ponto } = require("../commands");
+const { date, dateTime } = require("../../utils/dateTimeFormatter");
+const { clockIn, getClockIn } = require("../../utils/clockIn");
+
+const ERROR_MESSAGES = {
+  invalidDate: (userId) => ({
+    content: `Qualfoi <@${userId}>! Isso não é um horário válido, por favor usar o modelo \`HH:mm\`.\n`,
+    ephemeral: true,
+  }),
+  punchedTwoTimes: (userId) => ({
+    content: `>>> Mai é Zé dentro d\'água memo né, já bateu o ponto hoje mano.\n<@${userId}>`,
+    ephemeral: true,
+  }),
+};
+
+async function timeTableIsValid(option) {
+  const regex = "^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$";
+  return !!option.match(regex);
+}
+
+async function replyError(interaction, errorType) {
+  const userId = interaction.user.id;
+
+  await interaction.reply(ERROR_MESSAGES[errorType](userId));
+}
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("ponto")
-    .setDescription("Marca o horário de Início de trabalho.")
-    .addStringOption((option) =>
-      option
-        .setName("horario")
-        .setDescription("Coloca um horário especifíco")
-        .setRequired(false)
-    ),
+  data: ponto,
   async execute(interaction) {
-    let horarioOption = interaction.options.getString("horario");
+    const { user } = interaction;
 
-    let pontoUsuario = JSON.parse(await redis.getPonto(interaction.user.id));
-    const { DataHora, Data } = dataUtil;
+    const timeTableOption = interaction.options.getString("horario");
+    if (!timeTableOption && !timeTableIsValid)
+      replyError(interaction, "invalidDate");
 
-    if (pontoUsuario) {
-      let diaDoPonto = new Date(parseInt(pontoUsuario.data_ponto)).setHours(0, 0, 0, 0);
+    const userClockKey = await getClockIn(user.id);
+    
+    const timestampKey = parseInt(userClockKey.date);
+    const clockInDay = new Date(timestampKey).setHours(0, 0, 0, 0);
+    const todaysDay = new Date().setHours(0, 0, 0, 0);
 
-      let diaAtual = new Date().setHours(0, 0, 0, 0);
+    const clockInSchedule = timeTableOption
+      ? `${date()} - ${timeTableOption}`
+      : dateTime();
 
-      if (diaAtual > diaDoPonto) {
-        if (horarioOption) {
-          if (
-            horarioOption.match("^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$")
-          ) {
-            // dataHora = `${Data.Data} - ${horarioOption}`;
+    // verify if the user already punched the clock
+    if (userClockKey) {
+      // checks if the timetable from the punched clock is older than today
+      if (todaysDay > clockInDay) {
+        const { id } = await clockIn(interaction, clockInSchedule);
+        console.log(id);
 
-            const ponto = await discord.BateOPonto(interaction, `${Data} - ${horarioOption}`);
-            await discord.AdicionaReacoes(ponto);
-
-            console.log(ponto.id);
-          } else {
-            interaction.reply({
-              content: `Qualfoi! \`${horarioOption}\` não é um horário válido, por favor usar o modelo \`HH:mm\`.\n<@${interaction.user.id}>`,
-              ephemeral: true,
-            });
-          }
-        } else {
-          const ponto = await discord.BateOPonto(interaction, DataHora);
-          await discord.AdicionaReacoes(ponto);
-
-          console.log(ponto.id);
-        }
-      } else {
-        interaction.reply({
-          content: `>>> Mai é Zé dentro d\'água memo né, já bateu o ponto hoje mano.\n<@${interaction.user.id}>`,
-          ephemeral: true,
-        });
+        return;
       }
-    } else {
-      if (horarioOption) {
-        if (horarioOption.match("^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$")) {
-          const ponto = await discord.BateOPonto(
-            interaction,
-            `${Data} - ${horarioOption}`
-          );
-          await discord.AdicionaReacoes(ponto);
 
-          console.log(ponto.id);
-        } else {
-          interaction.reply({
-            content: `Qualfoi <@${interaction.user.id}>! \`${horarioOption}\` não é um horário válido, por favor usar o modelo \`HH:mm\`.\n`,
-            ephemeral: true,
-          });
-        }
-      } else {
-        const ponto = await discord.BateOPonto(interaction, DataHora);
-        await discord.AdicionaReacoes(ponto);
-
-        console.log(ponto.id);
-      }
+      await replyError(interaction, "punchedTwoTimes");
     }
+
+    const { id } = await clockIn(interaction, clockInSchedule);
+    console.log(id);
   },
 };

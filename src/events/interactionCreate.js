e9037e32f17d4de3934642135e0b2ee6b3600d38
thiagomsantanna <thiagomsantanna11@gmail.com>
2022-12-10 13:05:16 -0300

    refac: Removing index.js from commands folder when fs reading dir to locate registered commands

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,32 +1,33 @@
-const { Collection, Intents } = require('discord.js');
-const fs = require('fs');
-const path = require('path');
+const { Collection } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
-    name: 'interactionCreate',
-    once: false,
-    async execute(interaction) {
+  name: "interactionCreate",
+  once: false,
+  async execute(interaction) {
+    const client = interaction.client;
+    client.commands = new Collection();
 
-        const client = interaction.client;
-        client.commands = new Collection();
+    const commandFiles = fs
+      .readdirSync(path.resolve(__dirname, "../commands/"))
+      .filter((file) => file.endsWith(".js") && file !== "index.js");
 
-        const commandFiles = fs.readdirSync(path.resolve(__dirname, '../commands/')).filter(file => file.endsWith('.js'));
-        
-        for(const file of commandFiles){
-            const command = require(`../commands/${file}`);
-            client.commands.set(command.data.name, command);
-        }
-
-        if (!interaction.isCommand()) return;
-
-        const command = client.commands.get(interaction.commandName);
-
-        if (!command) return;
-
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(error);
-        }
+    for (const file of commandFiles) {
+      const command = require(`../commands/${file}`);
+      client.commands.set(command.data.name, command);
     }
-}
+
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};

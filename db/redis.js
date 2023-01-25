41aaf2106318e3b27c327da264ae622b33ab4c0f
thiagomsantanna <thiagomsantanna11@gmail.com>
2023-01-24 22:22:22 -0300

    refac: Refactoring the connection to redis

---
+++ db/redis.js
@@ -0,0 +1,15 @@
+const { createClient } = require("redis");
+
+const redisClient = process.env.REDIS_URL
+  ? createClient({ url: process.env.REDIS_URL })
+  : createClient();
+
+redisClient.on("error", (e) => console.error(e));
+redisClient.on("connect", () =>
+  console.log(`Redis connected on: ${process.env.REDIS_URL || "localhost"}`));
+
+(async () => {
+  await redisClient.connect();
+})();
+
+module.exports = redisClient;

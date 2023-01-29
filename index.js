a312f1c65358290e0b3b97b8cc6ffbcee10d3a7b
thiagomsantanna <thiagomsantanna11@gmail.com>
2023-01-29 20:06:38 -0300

    refac: Updating index.js to work with new version of discord,js

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const fs = require('fs');
-const { Client, Intents } = require('discord.js');
+require("dotenv").config({
+  path: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env",
+});
+const fs = require("fs");
+const { Client } = require("discord.js");
+
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
-    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+  intents: ["Guilds", "GuildMessages", "GuildMessageReactions"],
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
 });
 
-const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
+const eventFiles = fs
+  .readdirSync("./src/events")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of eventFiles) {
+  const event = require(`./src/events/${file}`);
 
-	const event = require(`./src/events/${file}`);
-
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
 }
-
 
 client.login(process.env.BOT_TOKEN);
